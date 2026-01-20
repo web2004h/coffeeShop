eb177dc7a5ecf5d64310fcef0be626688145adec
web2004h <2004alariqi@gmail.com>
2026-01-20 17:36:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+//Close menu when the close button is click
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu")
+});
+menuCloseButton.addEventListener("click", () => menuOpenButton.click()); 
+
+
+//Close menu when the nav link is click
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
+
+//Initialize Swiper
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween:25,
+
+  // If we need pagination
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+  // responsive breackpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
